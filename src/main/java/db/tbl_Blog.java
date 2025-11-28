d39d2ac24de9b79e07ba9c0875c395c1840a348e
TaiAnh88 <tranthienbao050@gmail.com>
2025-11-28 16:03:59 +0700

    ok

---
+++ src/main/java/db/tbl_Blog.java
@@ -0,0 +1,146 @@
+package db;
+import java.util.Date;
+public class tbl_Blog{
+	private int blogId;
+	private int accountId;
+	private String title;
+	private String alias;
+	private int categoryId;
+	private String description;
+	private String detail;
+	private String image;
+	private String seotitle;
+	private String seodescription;
+	private String seokeywords;
+	private Date createdate;
+	private String createby;
+	private Date modifieddate;
+	private String modifiedby;
+	private boolean isactive;
+	public tbl_Blog(int blogId, int accountId, String title, String alias, int categoryId, String description,
+			String detail, String image, String seotitle, String seodescription, String seokeywords, Date createdate,
+			String createby, Date modifieddate, String modifiedby, boolean isactive) {
+		super();
+		this.blogId = blogId;
+		this.accountId = accountId;
+		this.title = title;
+		this.alias = alias;
+		this.categoryId = categoryId;
+		this.description = description;
+		this.detail = detail;
+		this.image = image;
+		this.seotitle = seotitle;
+		this.seodescription = seodescription;
+		this.seokeywords = seokeywords;
+		this.createdate = createdate;
+		this.createby = createby;
+		this.modifieddate = modifieddate;
+		this.modifiedby = modifiedby;
+		this.isactive = isactive;
+	}
+	public int getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(int blogId) {
+		this.blogId = blogId;
+	}
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getSeotitle() {
+		return seotitle;
+	}
+	public void setSeotitle(String seotitle) {
+		this.seotitle = seotitle;
+	}
+	public String getSeodescription() {
+		return seodescription;
+	}
+	public void setSeodescription(String seodescription) {
+		this.seodescription = seodescription;
+	}
+	public String getSeokeywords() {
+		return seokeywords;
+	}
+	public void setSeokeywords(String seokeywords) {
+		this.seokeywords = seokeywords;
+	}
+	public Date getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+	public String getCreateby() {
+		return createby;
+	}
+	public void setCreateby(String createby) {
+		this.createby = createby;
+	}
+	public Date getModifieddate() {
+		return modifieddate;
+	}
+	public void setModifieddate(Date modifieddate) {
+		this.modifieddate = modifieddate;
+	}
+	public String getModifiedby() {
+		return modifiedby;
+	}
+	public void setModifiedby(String modifiedby) {
+		this.modifiedby = modifiedby;
+	}
+	public boolean isIsactive() {
+		return isactive;
+	}
+	public void setIsactive(boolean isactive) {
+		this.isactive = isactive;
+	}
+	@Override
+	public String toString() {
+		return "tbl_Blog [blogId=" + blogId + ", accountId=" + accountId + ", title=" + title + ", alias=" + alias
+				+ ", categoryId=" + categoryId + ", description=" + description + ", detail=" + detail + ", image="
+				+ image + ", seotitle=" + seotitle + ", seodescription=" + seodescription + ", seokeywords="
+				+ seokeywords + ", createdate=" + createdate + ", createby=" + createby + ", modifieddate="
+				+ modifieddate + ", modifiedby=" + modifiedby + ", isactive=" + isactive + "]";
+	}
+	
+}
---
+++ src/main/java/db/tbl_BlogComment.java
@@ -0,0 +1,80 @@
+package db;
+import java.util.Date;
+public class tbl_BlogComment{
+	private int commentId;
+	private String name;
+	private String phone;
+	private String email;
+	private Date createddate;
+	private String detail;
+	private int blogId;
+	private boolean isactive;
+	public tbl_BlogComment(int commentId, String name, String phone, String email, Date createddate, String detail,
+			int blogId, boolean isactive) {
+		super();
+		this.commentId = commentId;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.createddate = createddate;
+		this.detail = detail;
+		this.blogId = blogId;
+		this.isactive = isactive;
+	}
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreateddate() {
+		return createddate;
+	}
+	public void setCreateddate(Date createddate) {
+		this.createddate = createddate;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public int getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(int blogId) {
+		this.blogId = blogId;
+	}
+	public boolean isIsactive() {
+		return isactive;
+	}
+	public void setIsactive(boolean isactive) {
+		this.isactive = isactive;
+	}
+	@Override
+	public String toString() {
+		return "tbl_BlogComment [commentId=" + commentId + ", name=" + name + ", phone=" + phone + ", email=" + email
+				+ ", createddate=" + createddate + ", detail=" + detail + ", blogId=" + blogId + ", isactive="
+				+ isactive + "]";
+	}
+	
+	
+}
